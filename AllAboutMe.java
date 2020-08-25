4871df1a6ee6663823355ca1ea23bb70fc4734e3
pomeroyk <69909104+pomeroyk@users.noreply.github.com>
2020-08-25 12:48:21 -0400

    Create AllAboutMe.java

---
+++ AllAboutMe.java
@@ -0,0 +1,16 @@
+//this is a "get to know you" project - so please complete it with that intention in mind!
+public class AllAboutMe{
+  public static void main(String[] Args)
+  {
+    //declare and initialize a variable of the appropriate type to store these facts about you: name, grade in school, # of siblings, birthday month, favorite subject, favorite hobby
+    
+    
+    
+    
+    
+    //using System.out.println(), System.out.print(), your variables, and the concatenation operator (+), print out a nicely formatted paragraph describing yourself.
+    
+    
+    
+    }
+    }
